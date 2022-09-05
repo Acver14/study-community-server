e5b73f77a57f03382187708256740c9efaa0425d
Acver <devacver@gmail.com>
2022-09-05 20:20:56 +0900

    [add] init project

---
+++ src/main/java/com/kbb/studycommunity/StudyCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.kbb.studycommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbb/studycommunity/StudyCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbb.studycommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
